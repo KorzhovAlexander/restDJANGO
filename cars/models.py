eb4d5b92e304e23d68ecc1e0a453ca4a6f41a37c
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 01:09:56 +0300

    create full model "cars"

--- cars/models.py
+++ cars/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User=get_user_model()
+User = get_user_model()
+
 
 # Create your models here.
 class Car(models.Model):
     vin = models.CharField(verbose_name='Vin', db_index=True, unique=True, max_length=24)
     color = models.CharField(verbose_name='Color', db_index=True, max_length=24)
     brand = models.CharField(verbose_name='Brand', db_index=True, max_length=24)
-    CAR_TYPES=(
+    CAR_TYPES = (
         (1, 'Седан'),
         (2, 'Седан'),
         (3, 'Седан')
     )
-    car_type=models.IntegerField(verbose_name='Car_type', choices=CAR_TYPES)
-    user=models.ForeignKey(User,verbose_name='User', on_delete=models.CASCADE())
-
+    car_type = models.IntegerField(verbose_name='Car_type', choices=CAR_TYPES)
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)

51d7e4ab396f2b738a8f35a3d6e8151e986ec77b
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 21:25:05 +0300

    response print

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from cars.models import Car
 from .serializers import CarDetailSer, CarsSer
@@ -11,6 +12,10 @@ from .permissions import IsOwnerOrReadOnly
 
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSer
+
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        return Response
 
 
 class CarsView(generics.ListAPIView):

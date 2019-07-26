e7246960a8c66d165d4600bd3b34d6f16d7fdbbe
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 17:00:41 +0300

    create url for getAllCars

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from cars.views import CarCreateView
+from cars.views import CarCreateView, CarsView
 
 urlpatterns = [
     path('create/', CarCreateView.as_view()),
+    path('list/', CarsView.as_view()),
+
 ]

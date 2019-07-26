597fc5da379bb858377548c4fddd6c4aa0a13608
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 17:29:02 +0300

    url for car <int:pk>

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from cars.views import CarCreateView, CarsView
+from cars.views import CarCreateView, CarsView, CarDetailView
 
 urlpatterns = [
     path('create/', CarCreateView.as_view()),
     path('list/', CarsView.as_view()),
-
+    path('car-<int:pk>/', CarDetailView.as_view()),
 ]

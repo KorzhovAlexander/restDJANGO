70ef9cb01684fba48f8aef4374617d4982e65ae3
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 17:01:10 +0300

    create serializer for getAllCars

--- cars/serializers.py
+++ cars/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from cars.models import Car
 
 
+class CarsSer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ('id', 'vin', 'user')
+
+
 class CarDetailSer(serializers.ModelSerializer):
     class Meta:
         model = Car

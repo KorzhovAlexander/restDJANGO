3bafbaa8ae090b38a23a172ea27114dd073de9b5
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 20:54:43 +0300

    djoser create token auth url

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', include('cars.urls')),
     path('auth/', include('rest_framework.urls')),
+    path('auth_token/', include('djoser.urls.authtoken')),
 
 ]

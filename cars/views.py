e7819467d7bf3445daf935611f5d88dbacbf52c5
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-26 17:00:56 +0300

    create view for getAllCars

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import CarDetailSer
+
+from cars.models import Car
+from .serializers import CarDetailSer, CarsSer
 
 
 # Create your views here.
 
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSer
+
+
+class CarsView(generics.ListAPIView):
+    serializer_class = CarsSer
+    queryset = Car.objects.all()
